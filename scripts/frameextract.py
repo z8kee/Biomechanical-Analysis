b4bfd9e35fb2c3b24f09c95183c90566e72bfe6e
Zeke <144249438+z8kee@users.noreply.github.com>
2026-02-02 15:24:58 +0000

    Refactor frame extraction
    Refactor video feature extraction to use a function and include error handling. Added torch for tensor conversion and updated CSV writing logic.

--- scripts/frameextract.py
+++ scripts/frameextract.py
@@ -2,7 +2,7 @@
 #the npy files can then be used for phase classification models
 #this will be used everytime new videos are added to the dataset
 
-import os, cv2, csv, numpy as np, mediapipe as mp
+import os, cv2, csv, numpy as np, mediapipe as mp, torch
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
@@ -13,6 +13,7 @@ video_dir = 'data/video'
 out_dir = 'data/npy'
 m_path = 'models/pose_landmarker_full.task'
 meta_path = 'data/metadata.csv'
+temp_path = 'data/tempdata.csv'
 os.makedirs(out_dir, exist_ok=True)
 
 #----------make a model for mediapipe---------
@@ -20,72 +21,142 @@ baseOptions = python.BaseOptions
 poseLandmarker = vision.PoseLandmarker
 poseLandmarkerOptions = vision.PoseLandmarkerOptions
 visionRunningMode = vision.RunningMode
-meta_file = open(meta_path, 'w', newline='')
-writer = csv.writer(meta_file)
-writer.writerow(['sample_id', 'video_file', 'start_frame', 'end_frame'])
-option = poseLandmarkerOptions(
-    base_options=baseOptions(model_asset_path=m_path),
-    running_mode = visionRunningMode.VIDEO,
-    num_poses=1
-)
 
-sample_id = 0
 
-#----------------process videos and frames from videos into npy file dir--------------------
-
-for vid in os.listdir(video_dir): # for every vid in video directory, add to frames and save to npy file
-    print(f"Processing video: {vid}")
+def extract_features_for_app(video_path):
+    base_options = python.BaseOptions(model_asset_path=m_path)
     options = vision.PoseLandmarkerOptions(
-        base_options=python.BaseOptions(model_asset_path=m_path),
+        base_options=base_options,
         running_mode=vision.RunningMode.VIDEO,
         num_poses=1
     )
-    pose_landmarker = vision.PoseLandmarker.create_from_options(options)
-    vidcap = cv2.VideoCapture(os.path.join(video_dir, vid))
+    
+    try:
+        landmarker = vision.PoseLandmarker.create_from_options(options)
+    except:
+        return None, 0, 0
+
+    vidcap = cv2.VideoCapture(video_path)
     fps = vidcap.get(cv2.CAP_PROP_FPS)
-    if fps == 0:
-        fps = 30
-
-    frames = []
+    if fps == 0: fps = 30
+    
+    frames_data = []
     frame_idx = 0
-
+    
     while vidcap.isOpened():
         ret, frame = vidcap.read()
-        if not ret:
-            break
+        if not ret: break
 
-        mp_img = mp.Image(image_format=mp.ImageFormat.SRGB,
-                          data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        )
-
+        mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         ts_ms = int((frame_idx / fps) * 1000)
-        frame_idx += 1
-        if frame_idx % 30 == 0:
-            print(f"Processing frame {frame_idx} of video {vid}")
-        result = pose_landmarker.detect_for_video(mp_img, ts_ms)
+        result = landmarker.detect_for_video(mp_img, ts_ms)
 
         if result.pose_landmarks:
             landmarks = []
             for landmark in result.pose_landmarks[0]:
                 landmarks.extend([landmark.x, landmark.y, landmark.z, landmark.visibility])
-            frames.append(landmarks)
+            frames_data.append(landmarks)
         else:
-            if len(frames) > 0:
-                frames.append(frames[-1])
-            else:
-                frames.append([0.0]*132)
+            # zero padding / forward fill if missing
+            if len(frames_data) > 0: 
+                frames_data.append(frames_data[-1])
+            else: 
+                frames_data.append([0.0]*132)
+        
+        frame_idx += 1
 
     vidcap.release()
-    frames = np.array(frames)
-    print(f"Finished {vid}, total frames used: {len(frames)}")
+    
+    # window logic
+    window_size = 30
+    stride = 15
+    data_windows = []
+    frames_arr = np.array(frames_data)
 
-    #-------------windowing time---------------------
-    if len(frames) >= window:
-        for i in range(0, len(frames) - window+1, stride):
-            window_data = frames[i:i + window]
-            np.save(f"{out_dir}/sample{sample_id}.npy", window_data)
-            writer.writerow([sample_id, vid, i, i + window])
+    if len(frames_arr) < window_size:
+        return None, fps, frame_idx
 
-            sample_id += 1
+    for i in range(0, len(frames_arr) - window_size + 1, stride):
+        win = frames_arr[i : i + window_size]
+        data_windows.append(win)
+
+    if len(data_windows) > 0:
+        return torch.tensor(np.array(data_windows), dtype=torch.float32), fps, frame_idx
     else:
-        print(f"Video {vid} skipped due to insufficient frames.")
+        return None, fps, frame_idx
+
+def top_n(nums, n=3):
+    if not nums:
+        return []
+    return sorted(nums, reverse=True)[:n]  
+
+#----------------process videos and frames from videos into npy file dir--------------------
+if __name__ == "__main__":
+    option = poseLandmarkerOptions(
+    base_options=baseOptions(model_asset_path=m_path),
+    running_mode = visionRunningMode.VIDEO,
+    num_poses=1
+    )
+
+    sample_id = 360
+
+    meta_file = open(meta_path, 'w', newline='')
+    writer = csv.writer(temp_path)
+    writer.writerow(['sample_id', 'video_file', 'start_frame', 'end_frame'])
+
+    for vid in os.listdir(video_dir): # for every vid in video directory, add to frames and save to npy file
+        print(f"Processing video: {vid}")
+        options = vision.PoseLandmarkerOptions(
+            base_options=python.BaseOptions(model_asset_path=m_path),
+            running_mode=vision.RunningMode.VIDEO,
+            num_poses=1
+        )
+        pose_landmarker = vision.PoseLandmarker.create_from_options(options)
+        vidcap = cv2.VideoCapture(os.path.join(video_dir, vid))
+        fps = vidcap.get(cv2.CAP_PROP_FPS)
+        if fps == 0:
+            fps = 30
+
+        frames = []
+        frame_idx = 0
+
+        while vidcap.isOpened():
+            ret, frame = vidcap.read()
+            if not ret:
+                break
+
+            mp_img = mp.Image(image_format=mp.ImageFormat.SRGB,
+                            data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            )
+
+            ts_ms = int((frame_idx / fps) * 1000)
+            frame_idx += 1
+            if frame_idx % 30 == 0:
+                print(f"Processing frame {frame_idx} of video {vid}")
+            result = pose_landmarker.detect_for_video(mp_img, ts_ms)
+
+            if result.pose_landmarks:
+                landmarks = []
+                for landmark in result.pose_landmarks[0]:
+                    landmarks.extend([landmark.x, landmark.y, landmark.z, landmark.visibility])
+                frames.append(landmarks)
+            else:
+                if len(frames) > 0:
+                    frames.append(frames[-1])
+                else:
+                    frames.append([0.0]*132)
+
+        vidcap.release()
+        frames = np.array(frames)
+        print(f"Finished {vid}, total frames used: {len(frames)}")
+
+        #-------------windowing time---------------------
+        if len(frames) >= window:
+            for i in range(0, len(frames) - window+1, stride):
+                window_data = frames[i:i + window]
+                np.save(f"{out_dir}/sample{sample_id}.npy", window_data)
+                writer.writerow([sample_id, vid, i, i + window])
+
+                sample_id += 1
+        else:
+            print(f"Video {vid} skipped due to insufficient frames.")
